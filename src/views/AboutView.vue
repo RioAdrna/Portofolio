3aeb3171f303ad28c4da8d9d1c56cf15f9d569c7
R.Adrna <fikarugamerz123@gmail.com>
2025-02-18 23:55:48 +0700

    edit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -99,7 +99,7 @@ export default {
           id: 3,
           name: "Visual Code",
           imageUrl:
-            "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png",
           status: "Code Editor",
         },
         {
