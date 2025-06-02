f3fc881eaf505be940861156e30ffca63e2b27cd
R.Adrna <fikarugamerz123@gmail.com>
2025-06-02 21:52:18 +0700

    update logo3

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">RioAdriana();</span>
         </button>
         <div class="flex md:order-2 fadein-bot">
-          <a href="https://github.com/RioAdrna"><img class="w-9 rounded-full" src="https://icon2.cleanpng.com/20180617/cif/kisspng-gitlab-github-continuous-integration-computer-icon-5b25ebd5a92146.0530929015292118616928.jpg" alt="github"></a>
+          <a href="https://github.com/RioAdrna"><img class="w-9 rounded-full" src="https://img.icons8.com/m_outlined/512/github.png" alt="github"></a>
         </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
