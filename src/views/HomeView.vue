c2eb956c4a7bbe59bb378a97f88d4c2e94bbdd0b
R.Adrna <fikarugamerz123@gmail.com>
2025-07-06 15:47:18 +0700

    update pp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,7 +39,7 @@
         decoding="async"
         data-nimg="1"
         class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict"
-        src="/img/rio6.jpg">
+        src="/img/ryo.jpg">
     </div>
   </main>
   </template>
